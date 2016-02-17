9e2b1643ecbdf74eb3e09425a039af4296282d03
Narendra-mindstix <dilip.kumar@mindstix.com>
2016-02-17 19:50:16 +0530

    Some changes in index.html

--- controller/app.js
+++ controller/app.js
@@ -4,5 +4,6 @@ app.config(['$routeProvider',function($routeProvider){
 	when("/Animation",{templateUrl:'pages/animate.html'}).
 	when("/Change case",{templateUrl:"pages/change case.html"}).
 	when("/Include file content",{templateUrl:"pages/include file content.html"});
+	
 }]);
 
